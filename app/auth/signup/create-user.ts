5893b4604ddc5a228ad747a84f40db09c1fea313
ZakharchenkoStanislavIvanovich <staszaharchenko.sz@gmail.com>
2025-11-28 00:06:17 +0200

    Added UI Login auth

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,11 +1,12 @@
 "use server"
 
+import { FormError } from "@/app/common/form-error.interface";
 import { API_URL } from "@/app/constants/api"
 import { getErrorMessage } from "@/util/errors";
 import { post } from "@/util/fetch";
 import { redirect } from "next/navigation";
 
-export default async function createUser(_prevState: any, formData: FormData) {
+export default async function createUser(_prevState: FormError, formData: FormData) {
     const { error } = await post("users", formData);
     if (error) {
         return { error }
