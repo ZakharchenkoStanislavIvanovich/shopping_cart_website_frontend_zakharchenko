9da4dc7b81e90dcb11e078ab0978ea41a16b36d6
ZakharchenkoStanislavIvanovich <staszaharchenko.sz@gmail.com>
2025-12-01 22:21:11 +0200

    Added logout and navigation

--- app/auth/login/login.ts
+++ app/auth/login/login.ts
@@ -6,6 +6,7 @@ import { jwtDecode } from "jwt-decode";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { getErrorMessage } from "@/util/errors";
+import { AUTHENTICATION_COOKIE } from "../auth-cookie";
 
 export default async function login(_prevState: FormError, formData: FormData) {
     const res = await fetch(`${API_URL}/auth/login`, {
@@ -27,7 +28,7 @@ const setAuthCookie = async (response: Response) => {
     const token = setCookieHeader.split(";")[0]?.split("=")[1];
     if (token) {
       (await cookies()).set({
-        name: "Authentication",
+        name: AUTHENTICATION_COOKIE,
         value: token,
         secure: true,
         httpOnly: true,
